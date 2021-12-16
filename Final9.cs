77a707f2bdce6862186c67c30b405309b5d4695d
DESKTOP-PI164O8\Admin <pearmonday>
2021-12-16 12:27:07 +0700

    ข้อ9

---
+++ Final9.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Final_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input skill name : ");
+            string name = Console.ReadLine();
+            while (name != "?")
+            {
+                Node node1 = new Node(name);
+                Console.Write("Add dependency for {0} ? (Y/N) : ", name);
+                char YesorNo = char.Parse(Console.ReadLine());
+                if (YesorNo == 'Y' || YesorNo == 'y')
+                {
+                    Console.Write("Input skill name : ");
+                    name = Console.ReadLine();
+                    node1.nextNode = node1;
+                }
+                else if (YesorNo == 'N' || YesorNo == 'n')
+                {
+                    Console.WriteLine("No");
+                }
+                else
+                    ;
+
+            }
+
+        }
+    }
+    class Node
+    {
+        public string name;
+        public Node nextNode;
+
+        public Node(string nameValue)
+        {
+
+            name = nameValue;
+            nextNode = null;
+
+        }
+    }
+}
