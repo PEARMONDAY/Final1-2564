31bca9163a14465e285b8005a2375f4533b638b5
DESKTOP-PI164O8\Admin <pearmonday>
2021-12-16 12:26:57 +0700

    ข้อ8

---
+++ Final8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Final8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+            int A = int.Parse(Console.ReadLine());
+            char[] Number = new char[A];
+
+            Console.WriteLine("Input char : ");
+            for (int i = 0; i < A; i++)
+            {
+                Console.Write("{0}.", i + 1);
+                Number[i] = char.Parse(Console.ReadLine());
+            }
+            int Index;
+            for (int i = 0; i < A; i++)
+            {
+                Index = i;
+                for (int j = i; j < A; j++)
+                {
+                    Index = j;
+                }
+                Swap(ref Number, i, Index);
+            }
+            Console.Write("Summation : ");
+            for (int i = 0; i < A; i++)
+            {
+                Console.Write("{0},", Number[i]);
+            }
+        }
+        static void Swap(ref char[] number, int index1, int index2)
+        {
+            char tmp = number[index1];
+            number[index1] = number[index2];
+            number[index2] = tmp;
+        }
+    }
+}
